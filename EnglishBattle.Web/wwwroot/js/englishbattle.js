8c90cbf65392fa6658dba2b0eaf979d51144a9a2
maxdandres <max.dandres@gmail.com>
2022-11-14 10:13:14 -0500

    Add game UI + javascript files

---
+++ EnglishBattle.Web/wwwroot/js/englishbattle.js
@@ -0,0 +1,158 @@
+﻿function EnglishBattle(idPartie, niveau, baseVerbale, reponseP, reponsePP) {
+
+    _this = this;
+    this.idPartie = idPartie;
+    this.niveau = niveau;
+
+    this.score = 0;
+
+    this.inProgress = false;
+    this.infiniteLife = false;
+
+    this.wordList = [];
+
+    this.baseVerbale = baseVerbale  // DOM input element
+    this.reponseP = reponseP;       // DOM input element
+    this.reponsePP = reponsePP;     // DOM input element
+    // timestamps de reception/envoi des v
+    this.firstTimestamp = "";
+
+    this.wordIndex = 0;
+    this.hintIndex = 1;
+}
+
+EnglishBattle.prototype.Shuffle = function (array) {
+
+    var currentIndex = array.length;
+    var tempValue;
+    var randomIndex;
+
+    while (0 !== currentIndex) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        tempValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = tempValue;
+    }
+
+    return (array);
+}
+
+EnglishBattle.prototype.Init = function (verbesJSON) {
+
+    var tmp = JSON.parse(verbesJSON);
+    this.wordList = this.Shuffle(tmp);
+}
+
+EnglishBattle.prototype.Start = function (timestamp) {
+
+    this.inProgress = true;
+    // Affichage du mot à trouver
+    this.baseVerbale.val(this.wordList[this.wordIndex++].baseVerbale);
+    // Affichage des 3 prochains mots
+    for (; this.hintIndex <= 3; this.hintIndex++) {
+        this.NextHint(this.wordList[this.hintIndex].baseVerbale);
+    }
+    // Le jeu a commencé a ce point, récupération du premier timestamp pour la question #1
+    this.firstTimestamp = timestamp;
+}
+
+EnglishBattle.prototype.GetReponse = function (lastTimestamp) {
+
+    var reponse = JSON.stringify({
+        reponse: {
+            idPartie: this.idPartie,
+            idVerbe: this.GetCurrentVerbe().id,
+            //PP et P inversés car ils le sont également en base
+            reponsePreterit: this.reponsePP.val(),
+            reponseParticipePasse: this.reponseP.val(),
+            dateEnvoie: this.firstTimestamp,
+            dateReponse: lastTimestamp
+        }
+    });
+
+    return (reponse);
+}
+
+// Affiche le verbe suivant en prenant en parametre l'élément html qui affiche le verbe
+EnglishBattle.prototype.NextWord = function () {
+    // S'il n'y a plus de mot, retourne false pour signifier la fin de la partie
+    if (this.wordIndex >= this.wordList.length)
+        return (false);
+
+    var nextWord = " ";
+    var nextHint = "&nbsp";
+    // Selection du prochain mot
+    if (this.wordIndex < this.wordList.length) {
+        nextWord = this.wordList[this.wordIndex++].baseVerbale;
+    }
+
+    // Affectation de la nouvelle valeur de base verbale
+    this.baseVerbale.val(nextWord);
+
+    // Récupération du prochain hint
+    if (this.hintIndex < this.wordList.length)
+        nextHint = this.wordList[this.hintIndex++].baseVerbale
+    this.NextHint(nextHint);
+
+    return (true);
+}
+
+// Ajoute un nouveau verbe à la liste des prochains verbes qui vont sortir
+EnglishBattle.prototype.NextHint = function (nextHint) {
+    var toPrepend = `<div class="" style="opacity: 0.3;">${nextHint}</div>`;
+    // Ajout de la ligne avec une animation d'entrée en fondu
+    $(toPrepend).hide().prependTo($("#next-words")).fadeIn();
+}
+
+// Retourne l'id du verbe à renvoyer au serveur
+EnglishBattle.prototype.GetCurrentVerbe = function () {
+    var verbe = this.wordList[this.wordIndex - 1];
+    return (verbe);
+}
+
+// Check si les deux champs de réponse ne sont pas vide
+EnglishBattle.prototype.ReponseNotEmpty = function () {
+
+    var p = this.reponseP.val();
+    var pp = this.reponsePP.val();
+
+    if (p == "" || pp == "") {
+        // Met en evidence les inputs vides
+        if (p == "") {
+            this.reponseP.addClass("highlight-error");
+        }
+        if (pp == "") {
+            this.reponsePP.addClass("highlight-error");
+        }
+        return (false);
+    }
+    return (true);
+}
+
+// Nettoye les deux champs de réponse, et place le curseur sur la première réponse
+EnglishBattle.prototype.ClearInputs = function () {
+
+    this.reponseP.val("");
+    this.reponsePP.val("");
+    this.reponseP.focus();
+}
+
+EnglishBattle.prototype.TimeOut = function (timestamp) {
+
+    //console.log("timer out");
+    this.inProgress = false;
+    setTimeout(function () { __doPostBack("GAME_OVER", timestamp); }, 1);
+}
+
+EnglishBattle.prototype.UpdateScore = function (scoreHtmlElement) {
+    if (this.inProgress) {
+        this.score++;
+        scoreHtmlElement.html(this.score);
+    }
+}
+
+EnglishBattle.prototype.SetInfinite = function () {
+    this.infiniteLife = true;
+}
---
+++ EnglishBattle.Web/wwwroot/js/timer.js
@@ -0,0 +1,162 @@
+﻿// Code stoled from internet:
+// https://jsfiddle.net/prafuitu/xRmGV/
+
+function Timer(seconds = 60) {
+
+    _this = this;
+    _this.timer;
+    _this.duration = seconds;
+    _this.tick = 16;
+
+    _this.endCallBack = null;
+
+    _this.radius = 60;
+    _this.offset = 10;
+
+    _this.col_H = 120;
+    _this.col_S = 95;
+    _this.col_L = 48;
+
+    _this.miliseconds = null;
+
+    // Creation de la base graphique du Timer
+    for (var x = 0; x < 24; x++) {
+        var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+
+        path.setAttributeNS(null, 'd', 'M70,70 L70,10 A60,60 0 0,1 77.3121,10.4472');
+        path.setAttributeNS(null, 'fill', '#333');
+        path.setAttributeNS(null, 'fill-opacity', 0.3);
+        path.setAttributeNS(null, 'stroke', '#333');
+        path.setAttributeNS(null, 'stroke-width', 1);
+        path.setAttributeNS(null, 'stroke-opacity', 0.4);
+        path.setAttributeNS(null, 'transform', 'rotate(' + ((x * 15) - 4) + ' 70 70)');
+    }
+}
+
+// Retourne une string au format DateTime 'YYYY-MM-DD HH:mm:ss'
+Timer.prototype.getTimeStamp = function () {
+    var today = new Date();
+    // Correctif pour le mois, car il va de 0 à 11
+    var month = (today.getMonth() + 1) < 10 ? `0${today.getMonth() + 1}` : (today.getMonth() + 1);
+    var date = `${today.getFullYear()}-${month}-${today.getDate()}`;
+    var time = `${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`;
+
+    return (`${date} ${time}`);
+}
+
+// Fonction en rapport avec la couleur du cadran ?
+Timer.prototype.hue2rgb = function (t1, t2, t3) {
+    if (t3 < 0) t3 += 1;
+    if (t3 > 1) t3 -= 1;
+
+    if (t3 * 6 < 1) return t2 + (t1 - t2) * 6 * t3;
+    if (t3 * 2 < 1) return t1;
+    if (t3 * 3 < 2) return t2 + (t1 - t2) * (2 / 3 - t3) * 6;
+
+    return t2;
+}
+
+// Fonction en rapport avec la couleur du cadran ?
+Timer.prototype.hsl2rgb = function (H, S, L) {
+    var R, G, B;
+    var t1, t2;
+
+    H = H / 360;
+    S = S / 100;
+    L = L / 100;
+
+    if (S == 0) {
+        R = G = B = L;
+    } else {
+        t1 = (L < 0.5) ? L * (1 + S) : L + S - L * S;
+        t2 = 2 * L - t1;
+
+        R = _this.hue2rgb(t1, t2, H + 1 / 3);
+        G = _this.hue2rgb(t1, t2, H);
+        B = _this.hue2rgb(t1, t2, H - 1 / 3);
+    }
+
+    return [
+        Math.round(R * 255),
+        Math.round(G * 255),
+        Math.round(B * 255)
+    ];
+}
+
+// Progression du timer
+Timer.prototype.drawCoord = function (radius, degrees) {
+    var radians = degrees * Math.PI / 180;
+
+    var rX = radius + _this.offset + Math.sin(radians) * radius;
+    var rY = radius + _this.offset - Math.cos(radians) * radius;
+
+    var dir = (degrees > 180) ? 1 : 0;
+
+    var coord = 'M' + (radius + _this.offset) + ',' + (radius + _this.offset) + ' ' +
+        'L' + (radius + _this.offset) + ',' + _this.offset + ' ' +
+        'A' + radius + ',' + _this.radius + ' 0 ' + dir + ',1 ' +
+        rX + ',' + rY;
+
+    return coord;
+}
+
+Timer.prototype.update = function (deg, sec) {
+    var RGB = [];
+    var draw = _this.drawCoord(_this.radius, deg);
+
+    $('#progress_3').attr('d', draw);
+
+    _this.col_H = 120 - Math.round(deg / 3);
+    RGB = _this.hsl2rgb(_this.col_H, _this.col_S, _this.col_L);
+
+    $('#progress_3').attr('fill', 'rgb(' + RGB.join(', ') + ')');
+    $('#seconds_3').html(sec);
+}
+
+// UPDATE (interval)
+Timer.prototype.updateTimer = function () {
+
+    var date = new Date();
+
+    if (_this.miliseconds == null) _this.miliseconds = date.getTime();
+
+    var diff = (date.getTime() - _this.miliseconds) % (1000 * _this.duration);
+
+    // Condition de fin du timer
+    if (diff + (_this.tick * 2) >= _this.duration * 1000) {
+        _this.stop();
+        $('#progress_3').attr('d', "0");
+        $('#seconds_3').html("0");
+
+        _this.endCallBack(_this.getTimeStamp());
+
+        return;
+    }
+
+    var degrees = 0.36 * diff / _this.duration;
+    var seconds = _this.duration - Math.floor(diff / 1000);
+
+    _this.update(degrees, seconds);
+}
+
+// La fonction start prend en parametre un callback qui sera appellé à la fin du timer
+Timer.prototype.start = function (endCallBack) {
+    _this.endCallBack = endCallBack;
+    _this.timer = setInterval(_this.updateTimer, _this.tick);
+}
+
+Timer.prototype.stop = function () {
+    clearInterval(this.timer);
+    _this.miliseconds = null;
+}
+
+Timer.prototype.refresh = function () {
+    _this.stop();
+    _this.start();
+}
+
+Timer.prototype.reset = function () {
+    _this.stop();
+    _this.miliseconds = null;
+    _this.start();
+}
